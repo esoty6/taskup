150be92dfd0167f9842588fc05f95de722926212
esoty6 <kamilwitek12@o2.pl>
2024-11-07 15:12:36 +0100

    computed values instead of getters

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,10 @@
-import { Component, signal, WritableSignal } from '@angular/core';
+import {
+  Component,
+  computed,
+  Signal,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { MOCK_USERS } from '../../mocks/mock-users';
 import { User } from '../../types/user.type';
 import { getRandomElement } from '../../utils/utlis';
@@ -11,22 +17,18 @@ import { getRandomElement } from '../../utils/utlis';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  private selectedUser: WritableSignal<User>;
-
-  constructor() {
-    this.selectedUser = signal<User>(getRandomElement(MOCK_USERS));
-  }
-
-  get userName(): string {
-    return this.selectedUser().name;
-  }
-
-  get imageSrc(): string {
-    return 'https://thispersondoesnotexist.com/'.concat(
+  private selectedUser: WritableSignal<User> = signal<User>(
+    getRandomElement(MOCK_USERS)
+  );
+  protected userName: Signal<string> = computed(() => this.selectedUser().name);
+  protected imageSrc: Signal<string> = computed(() =>
+    'https://thispersondoesnotexist.com/'.concat(
       '?ts=',
-      Date.now().toString()
-    );
-  }
+      Date.now().toString(),
+      '&user=',
+      this.selectedUser().id.toString()
+    )
+  );
 
   protected onUserClick(): void {
     this.selectedUser.set(getRandomElement(MOCK_USERS));
