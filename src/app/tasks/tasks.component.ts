d01a1288132eb4313c160e67724a6dca9b6c2530
esoty6 <kamilwitek12@o2.pl>
2024-11-07 19:05:38 +0100

    new task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,19 +1,21 @@
 import { Component, computed, input, signal } from '@angular/core';
 import { MOCK_TASKS } from '../../mocks/mock-tasks';
 import { type User } from '../user-list/user/user.model';
+import { NewTaskComponent } from './new-task/new-task.component';
 import { TaskComponent } from './task/task.component';
 import { type Task } from './task/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   readonly user = input.required<User>();
 
+  protected isAddingTask = signal<boolean>(false);
   protected tasks = signal<Task[]>(MOCK_TASKS);
 
   protected userTasks = computed<Task[]>(() =>
@@ -24,5 +26,11 @@ export class TasksComponent {
     this.tasks.update((tasks) => tasks.filter((task) => task.id !== taskId));
   }
 
-  protected addTask(): void {}
+  protected onStartAddTask(): void {
+    this.isAddingTask.set(true);
+  }
+
+  protected onStopAddTask(): void {
+    this.isAddingTask.set(false);
+  }
 }
